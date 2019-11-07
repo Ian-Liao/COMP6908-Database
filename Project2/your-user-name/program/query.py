2bc088b7a10c09d7dd68994b950da425387b9e9b
Ian Liao <ian.liao@verafin.com>
2019-11-07 10:38:52 -0330

    switching from print result to write result to file&implementing the baseline of query e

--- Project2/your-user-name/program/query.py
+++ Project2/your-user-name/program/query.py
@@ -2,33 +2,36 @@ from relAlg import select, project, join
 
 
 def query_a():
-    select("Suppliers", "sid", "=", "s23")
-    project("Suppliers", ["sname"])
+    tmp_result = select("../data/Temporary/rel10.txt", "sid", "=", "s23")
+    query_result = project(tmp_result, ["sname"])
+    print(query_result)
 
 
 def query_b():
-    select("Suppliers", "sid", "=", "s23")
-    project("Suppliers", ["sname"])
+    tmp_result = select("../data/Temporary/rel10.txt", "sid", "=", "s23")
+    query_result = project(tmp_result, ["sname"])
+    print(query_result)
 
 
 def query_c():
-    join("Suppliers", "sid", "Supply", "sid")
     select("Supply", "pid", "=", "p15")
+    join("Suppliers", "sid", "Supply", "sid")
     project("Suppliers", ["address"])
 
 
 def query_d():
-    join("Suppliers", "sid", "Supply", "sid")
     select("Suppliers", "sname", "=", "Kiddie")
+    join("Suppliers", "sid", "Supply", "sid")
     project("Supply", ["cost"])
 
 
 def query_e():
-    pass
+    select("Supply", "cost", ">=", 47.00)
+    join("Supply", "pid", "Products", "pid")
+    join("Supply", "sid", "Suppliers", "sid")
+    project("tmp.txt", ["sname", "pname", "cost"])
 
 
 if __name__ == "__main__":
-    select("../data/Temporary/rel00.txt", "cost", ">=", 40)
-    select("../data/Temporary/rel00.txt", "cost", "=", 30.82)
-    project("../data/Temporary/rel00.txt", ["sid", "cost"])
-    join("../data/Temporary/rel00.txt", "pid", "../data/Temporary/rel01.txt", "pid")
+    query_a()
+
--- Project2/your-user-name/program/relAlg.py
+++ Project2/your-user-name/program/relAlg.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 
 def select(rel, att, op, val):
+    res = ""
     with open(rel) as f:
         content = f.readlines()[0]
         data = json.loads(content)
         df = pd.DataFrame(data[1:], columns=data[0])
-        print(df)
         if op == '<':
             df = df.loc[df[att] < val]
         elif op == '<=':
@@ -21,7 +21,12 @@ def select(rel, att, op, val):
         else:
             raise Exception('Invalid op value!!!')
         res = [df.columns.values.tolist()] + df.values.tolist()
-        print(res)
+
+    tmp_result = "../data/Temporary/tmp.txt"
+    with open(tmp_result, "w") as f:
+        f.write(json.dumps(res))
+
+    return tmp_result
 
 
 def project(rel, attList):
@@ -32,7 +37,12 @@ def project(rel, attList):
         df = df.filter(attList)
         df.drop_duplicates(keep=False, inplace=True)
         res = [df.columns.values.tolist()] + df.values.tolist()
-        print(res)
+
+    tmp_result = "../data/Temporary/tmp.txt"
+    with open(tmp_result, "w") as f:
+        f.write(json.dumps(res))
+
+    return tmp_result
 
 
 def join(rel1, att1, rel2, att2):
@@ -43,5 +53,11 @@ def join(rel1, att1, rel2, att2):
         data2 = json.loads(content2)
         df1 = pd.DataFrame(data1[1:], columns=data1[0])
         df2 = pd.DataFrame(data2[1:], columns=data2[0])
-        res = pd.merge(df1, df2, left_on=att1, right_on=att2, how='inner')
-        print(res)
+        df = pd.merge(df1, df2, left_on=att1, right_on=att2, how='inner')
+        res = [df.columns.values.tolist()] + df.values.tolist()
+
+    tmp_result = "../data/Temporary/tmp.txt"
+    with open(tmp_result, "w") as f:
+        f.write(json.dumps(res))
+
+    return tmp_result
